ef382114ff4f1ae68447dea23db435b2dc7a681f
Kareem El-Wishahy <kareem.elwishahy@gmail.com>
2023-01-21 16:44:52 -0800

    ux changes

--- frontend/textmemoirs/lib/pages/home.dart
+++ frontend/textmemoirs/lib/pages/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:chat_bubbles/chat_bubbles.dart';
+import '../widgets/button.dart';
+import 'package:http/http.dart' as http;
 
 const PrimaryColorLight = const Color(0xFFA1B0AB);
 
@@ -19,7 +21,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final messages = [];
-    for (var i = 0; i < 100; i++) {
+    for (var i = 99; i >= 0; i--) {
       messages.add({"text": "This is entry # $i", "date": DateTime.now()});
     }
     return Scaffold(
@@ -30,37 +32,53 @@ class _HomePageState extends State<HomePage> {
         body: Padding(
             padding: EdgeInsets.all(20),
             child: Scaffold(
-                backgroundColor: Colors.white,
-                body: SingleChildScrollView(
-                  child: Column(
+              backgroundColor: Colors.white,
+              body: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          const Text("Left"),
-                          DateChip(date: messages[0]["date"]),
-                          const Text("Right"),
-                        ],
+                      Container(
+                        alignment: Alignment.bottomRight,
+                        child: Button(
+                          buttonText: 'Previous',
+                          onPressed: () => {},
+                          colour: PrimaryColorLight,
+                        ),
                       ),
-                      ListView.builder(
-                          itemCount: messages.length,
-                          scrollDirection: Axis.vertical,
-                          shrinkWrap: true,
-                          itemBuilder: (context, index) {
-                            var msg = messages[index];
-                            return BubbleNormal(
-                              text: msg["text"],
-                              isSender: true,
-                              color: Color(0xFF1B97F3),
-                              tail: true,
-                              textStyle: const TextStyle(
-                                fontSize: 20,
-                                color: Colors.white,
-                              ),
-                            );
-                          })
+                      DateChip(date: messages[0]["date"]),
+                      Container(
+                        alignment: Alignment.bottomRight,
+                        child: Button(
+                          buttonText: 'Next',
+                          onPressed: () => {},
+                          colour: PrimaryColorLight,
+                        ),
+                      ),
                     ],
                   ),
-                ))));
+                  Expanded(
+                    child: ListView.builder(
+                        itemCount: messages.length,
+                        scrollDirection: Axis.vertical,
+                        shrinkWrap: true,
+                        reverse: true,
+                        itemBuilder: (context, index) {
+                          var msg = messages[index];
+                          return BubbleNormal(
+                            text: msg["text"],
+                            isSender: true,
+                            color: Color(0xFF1B97F3),
+                            tail: true,
+                            textStyle: const TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+                          );
+                        }),
+                  )
+                ],
+              ),
+            )));
   }
 }
